46b591e27e4be0c4fc8e358b13025d0d10fb6552
Youssef Najjarine <ynajjarine@gmail.com>
2025-08-28 20:47:23 -0500

    Draft 3.

--- chuckle-norris/src/app/components/home/home.ts
+++ chuckle-norris/src/app/components/home/home.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+    randomJoke: string = "When Chuck Norris looks in a mirror, the mirror shatters because not even glass is dumb enough to get in between Chuck Norris and Chuck Norris. When Chuck Norris looks in a mirror, the mirror shatters because not even glass is dumb enough to get in between Chuck Norris and Chuck Norris. When Chuck Norris looks in a mirror, the mirror shatters because not even glass is dumb enough to get in between Chuck Norris and Chuck Norris.";
+  constructor() {
 
+  }
 }
--- chuckle-norris/src/app/components/rank-joke/rank-joke.ts
+++ chuckle-norris/src/app/components/rank-joke/rank-joke.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './rank-joke.css'
 })
 export class RankJoke {
-
+  joke1: string = "Chuck Norris can slam a revolving door.";
+  joke2: string = "Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. ";
+  joke3: string = "Chuck Norris can speak Braille.";
 }
