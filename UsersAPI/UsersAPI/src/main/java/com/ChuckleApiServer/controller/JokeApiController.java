573e8e9c71983e48e9f9d728fc4afc7015bbb0b6
ZacharySanford <afavorhouse2113@gmail.com>
2025-09-04 19:43:58 -0400

    working Joke API

--- UsersAPI/UsersAPI/src/main/java/com/ChuckleApiServer/controller/JokeApiController.java
+++ UsersAPI/UsersAPI/src/main/java/com/ChuckleApiServer/controller/JokeApiController.java
@@ -5,18 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @RequestMapping
 public class JokeApiController {
 
-    JokeService theJokeService;
+    JokeService theJokeService = new JokeService();
 
 
 
     @GetMapping("/Joke")
     public Joke getJoke(){
+
         return  theJokeService.getRandomJoke();
     }
+
+
 
 
 }
