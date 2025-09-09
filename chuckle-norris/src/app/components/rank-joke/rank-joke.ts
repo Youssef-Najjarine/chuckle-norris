0390ca22f2f12106057c577a39570ab1c4208bad
Youssef Najjarine <ynajjarine@gmail.com>
2025-09-08 20:31:10 -0500

    Added changes to rank a joke.

--- chuckle-norris/src/app/components/rank-joke/rank-joke.ts
+++ chuckle-norris/src/app/components/rank-joke/rank-joke.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { JokeApiService } from '../../services/joke-api-service';
+import { concat } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-rank-joke',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './rank-joke.html',
   styleUrl: './rank-joke.css'
 })
-export class RankJoke {
-  joke1: string = "Chuck Norris can slam a revolving door.";
-  joke2: string = "Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. Chuck Norris can hear sign language. ";
-  joke3: string = "Chuck Norris can speak Braille.";
+export class RankJoke implements OnInit {
+    joke1 : any = {};
+    joke2: any = {};
+    joke3: any = {};
+    constructor(private jokeApiService: JokeApiService) {
+    }
+
+    async ngOnInit()
+    {
+      this.joke1 = await this.getNewJoke1();
+      this.joke2 = await this.getNewJoke2();
+      this.joke3 = await this.getNewJoke3();
+    }
+    async getNewJokes() {
+      this.joke1 = await this.getNewJoke1();
+      this.joke2 = await this.getNewJoke2();
+      this.joke3 = await this.getNewJoke3();
+      console.log("THE RANDOM JOKE: ", this.joke1, this.joke2, this.joke3);
+    }
+    async getNewJoke1() {
+      this.joke1 = await this.jokeApiService.getJoke();
+    }
+    async getNewJoke2() {
+        this.joke2 = await this.jokeApiService.getJoke();
+    }
+    async getNewJoke3() {
+      this.joke3 = await this.jokeApiService.getJoke();
+    }    
 }
